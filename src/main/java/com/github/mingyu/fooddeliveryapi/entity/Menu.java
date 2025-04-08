8e2cd47574a6a484fc9045a3716818cd244d56d1
kimmingyu94 <kmg3592@naver.com>
2025-04-08 19:14:09 +0900

    feat(menu) : 엔티티 및 Enum 추가

---
+++ src/main/java/com/github/mingyu/fooddeliveryapi/entity/Menu.java
@@ -0,0 +1,54 @@
+package com.github.mingyu.fooddeliveryapi.entity;
+
+import com.github.mingyu.fooddeliveryapi.enums.MenuStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "menu")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long menuId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "storeId", nullable = false)
+    private Store store;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private MenuStatus status;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MenuOption> menuOptions = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        modifiedDate = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/github/mingyu/fooddeliveryapi/entity/MenuOption.java
@@ -0,0 +1,49 @@
+package com.github.mingyu.fooddeliveryapi.entity;
+
+import com.github.mingyu.fooddeliveryapi.enums.MenuOptionStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "menu_options")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long menuOptionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menuId", nullable = false)
+    private Menu menu;
+
+    @Column(name = "option_name", nullable = false)
+    private String option;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false, length = 255)
+    private MenuOptionStatus status;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        modifiedDate = LocalDateTime.now();
+    }
+}
