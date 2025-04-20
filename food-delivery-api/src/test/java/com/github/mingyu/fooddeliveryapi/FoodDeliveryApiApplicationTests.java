66f80ab41e198433caa80db3a09906ef39a1941b
kimmingyu94 <kmg3592@naver.com>
2025-04-20 13:27:40 +0900

    프로젝트 멀티 모듈로 재구성

---
+++ food-delivery-api/src/test/java/com/github/mingyu/fooddeliveryapi/FoodDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mingyu.fooddeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
