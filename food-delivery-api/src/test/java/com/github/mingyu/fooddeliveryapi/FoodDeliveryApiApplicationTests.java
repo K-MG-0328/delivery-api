193d5b422a040e4001b7ce7d9b98abf649f21c05
kimmingyu94 <kmg3592@naver.com>
2025-04-27 17:12:40 +0900

    젠킨스 테스트

--- food-delivery-api/src/test/java/com/github/mingyu/fooddeliveryapi/FoodDeliveryApiApplicationTests.java
+++ food-delivery-api/src/test/java/com/github/mingyu/fooddeliveryapi/FoodDeliveryApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.mingyu.fooddeliveryapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FoodDeliveryApiApplicationTests {
 
     @Test
